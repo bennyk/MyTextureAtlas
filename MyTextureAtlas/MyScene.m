ab18129302ac02d4111bb2147bd65399d16c8628
Benny Khoo <benny_khoo_99@yahoo.com>
2014-06-16 14:47:29 +0800

    removing test code.

--- MyTextureAtlas/MyScene.m
+++ MyTextureAtlas/MyScene.m
@@ -52,18 +52,6 @@
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
-        
         [self initScene];
     }
     return self;
